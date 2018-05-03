d88460b5d485eb8a39217b3269c152c31891c8de
LABMat(i) <labmatiii@gmail.com>
2018-05-03 17:28:52 -0300

    Agora ele pula

--- Dino/js/Sketch.js
+++ Dino/js/Sketch.js
@@ -1,14 +1,20 @@
 function preload(){
 	img = loadImage('img/dinozao.png')
 }
+var cactusArray = []
+var canvas = {}
+
 function setup(){
 	canvas = createCanvas(innerWidth,innerHeight);
+	canvas.elt.click()
 	ground = height- 100
 	document.querySelector('div#interface').appendChild(canvas.elt);
 	dino = new Dino(100, 200, ground-100, ground-100, img)
 	dino.show()
 	frameRate(60)
 }
+
+var timerCactus = 0;
 
 function draw(){
 	background('e3e3e3');
@@ -17,6 +23,26 @@ function draw(){
 	line(0,ground,width,ground)
 	dino.update()
 	dino.show()
+	fill(49)
+	for (let i = 0; i < cactusArray.length; i++) {
+		cactusArray[i].show()		
+		if(!cactusArray[i].update()){
+			console.log('Entrou')
+			// cactusArray.shift()
+		}
+	}
+	timerCactus = random(1500,2500)
+}
+
+
+
+setTimeout(createCactus, timerCactus)
+
+
+function createCactus(){
+	cactus = new Cactus()
+	cactusArray.push(cactus)
+	setTimeout(createCactus, timerCactus)
 }
 
 class Dino{
@@ -42,9 +68,28 @@ class Dino{
 			dino.pya += dino.vy
 		}
 	}
+}
 
-	jump(){
+var escalaCactus = 40
 
+class Cactus{
+	constructor(){
+		this.height = Math.ceil((Math.random() * (4 - 1) + 1)) * escalaCactus
+		this.width = Math.ceil((Math.random() * (3 - 1) + 1)) * (escalaCactus/1.5)
+		this.px = canvas.width-60
+	}
+
+	show(){
+		fill('red')
+		rect(this.px, ground-this.height, this.width, this.height)
+	}
+
+	update(){
+		if(this.px > -90){
+			this.px -= 10;
+		}else{
+			return false	
+		}
 	}
 }
 
